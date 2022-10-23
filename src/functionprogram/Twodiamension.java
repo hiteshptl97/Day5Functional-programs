6e9e9d143e38ae01c59632b0131070725cb6e003
hiteshptl97 <hiteshpatil897@gmail.com>
2022-10-23 18:42:04 +0530

    Day 5 assignment complete

---
+++ src/functionprogram/Twodiamension.java
@@ -0,0 +1,40 @@
+package functionprogram;
+import java.util.Random;
+import java.util.Scanner;
+public class Twodiamension {
+
+
+
+
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter number of Rows (m)");
+            int m = scan.nextInt();
+            System.out.println("Enter number of columns (n)");
+            int n = scan.nextInt();
+            int[][] array = new int[m][n];
+            Twodiamension obj = new Twodiamension();
+            obj.outerArray(array);
+            obj.innerArray(array);
+        }
+
+        void outerArray(int[][] array) {
+            Random randomInt = new Random();
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    array[i][j] = randomInt.nextInt(100);
+                }
+            }
+        }
+
+        void innerArray(int[][] array) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    System.out.printf("%d ", array[i][j]);
+                }
+                System.out.println();
+            }
+        }
+    }
+
